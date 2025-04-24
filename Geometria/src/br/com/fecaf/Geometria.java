e156a0cc27daf379b4f573fad55d83aca66a0779
Paulo <88458042+garenks@users.noreply.github.com>
2025-04-24 14:37:38 -0300

    Add files via upload

---
+++ Geometria/src/br/com/fecaf/Geometria.java
@@ -0,0 +1,37 @@
+package br.com.fecaf;
+
+import br.com.fecaf.model.Retangulo;
+
+public class Geometria {
+    public static void main(String[] args) {
+        //instanciar Obejto Retangulo
+        Retangulo retangulo1 = new Retangulo();
+
+        retangulo1.nome = "retângulo-1";
+        retangulo1.lado1 = 10;
+        retangulo1.lado2= 5;
+
+        retangulo1.exibirInformacoes();
+        retangulo1.calcularArea();
+        retangulo1.calcularPerimetro();
+
+
+        //instanciar Obejto Retangulo 2
+        Retangulo retangulo2 = new Retangulo();
+
+        retangulo2.nome = "retângulo-2";
+        retangulo2.lado1 = 8;
+        retangulo2.lado2= 8;
+
+        retangulo2.exibirInformacoes();
+        retangulo2.calcularArea();
+        retangulo2.calcularPerimetro();
+
+    //validar se é um quadrado
+        retangulo1.validarRetanguloEspecial();
+        retangulo2.validarRetanguloEspecial();
+    }
+
+
+
+}
---
+++ Geometria/src/br/com/fecaf/model/Retangulo.java
@@ -0,0 +1,37 @@
+package br.com.fecaf.model;
+
+public class Retangulo {
+    public double lado1, lado2;
+
+    public double area;
+
+    public double perimetro;
+    public String nome;
+
+    public void exibirInformacoes(){
+        System.out.println("/-------------------------/");
+        System.out.println("O nome é : " + nome);
+        System.out.println("O lado1 é : " + lado1);
+        System.out.println("O lado2 é : " + lado2);
+        System.out.println("/-------------------------/");
+    }
+
+    public void calcularArea(){
+        area = lado1 * lado2;
+        System.out.println("Á area do objeto" + nome +  " é : " + area);
+
+    }
+    public void calcularPerimetro (){
+        perimetro = 2 * lado1 + 2 * lado2;
+        System.out.println("O perimetro do objeto " + nome + " é : " + perimetro);
+    }
+
+
+    public void validarRetanguloEspecial(){
+        if (lado1 == lado2){
+            System.out.println("O objeto" + nome + " É um retângulo especial (Quadrado) ...");
+        } else {
+            System.out.println("O objeto" + nome + " Não é um retângulo especial ...");
+        }
+    }
+}
