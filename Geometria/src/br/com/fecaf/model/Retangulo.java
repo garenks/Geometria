611dc08b314540dc9102bf3212b91df4ad1bfd07
Paulo <88458042+garenks@users.noreply.github.com>
2025-04-28 13:32:48 -0300

    Update Retangulo.java

--- Geometria/src/br/com/fecaf/model/Retangulo.java
+++ Geometria/src/br/com/fecaf/model/Retangulo.java
@@ -18,20 +18,28 @@ public class Retangulo {
 
     public void calcularArea(){
         area = lado1 * lado2;
+        System.out.println("/-------------------------/");
         System.out.println("Á area do objeto" + nome +  " é : " + area);
+        System.out.println("/-------------------------/");
 
     }
     public void calcularPerimetro (){
         perimetro = 2 * lado1 + 2 * lado2;
+        System.out.println("/-------------------------/");
         System.out.println("O perimetro do objeto " + nome + " é : " + perimetro);
+        System.out.println("/-------------------------/");
     }
 
 
     public void validarRetanguloEspecial(){
         if (lado1 == lado2){
+            System.out.println("/-------------------------/");
             System.out.println("O objeto" + nome + " É um retângulo especial (Quadrado) ...");
+            System.out.println("/-------------------------/");
         } else {
+            System.out.println("/-------------------------/");
             System.out.println("O objeto" + nome + " Não é um retângulo especial ...");
+            System.out.println("/-------------------------/");
         }
     }
 }
